37c84ba53303305b3158b511673033f206fbc17d
dev3 <nabendu.bose@pixelconsultancy.in>
2024-09-13 17:24:00 +0530

    update

--- api/controllers/email.controller.js
+++ api/controllers/email.controller.js
@@ -5,12 +5,37 @@ const puppeteer = require("puppeteer-core");
 let ejs = require("ejs");
 
 class Email {
+    // async sendEmail(req,res) {
+    //     const templatePath = path.join(__dirname, '../views/emailTemplate.ejs');
+    //     const html = await ejs.renderFile(templatePath);
+    //     try {
+    //         const browser = await puppeteer.launch({
+    //             args: chromium.args,
+    //             defaultViewport: chromium.defaultViewport,
+    //             executablePath: await chromium.executablePath(),
+    //             headless: chromium.headless,
+    //             ignoreHTTPSErrors: true,
+    //         });
+    //         const page = await browser.newPage();
+    //         await page.setContent(html, { waitUntil: 'networkidle0' });
+    //         const pdfBuffer = await page.pdf();
+    //         await browser.close();
+    //         res.send(pdfBuffer);
 
-    async sendEmail(req,res) {
-        // const html = fs.readFileSync(path.resolve("../views/emailTemplate.ejs"), "utf8");
+    //     } catch (error) {
+    //         console.error(error);
+    //         res.send(error);
+    //     }
+    // }
+
+    async sendEmail(req, res) {
         const templatePath = path.join(__dirname, '../views/emailTemplate.ejs');
-        const html = await ejs.renderFile(templatePath);
+
         try {
+            // Render the EJS template
+            const html = await ejs.renderFile(templatePath);
+
+            // Launch Puppeteer with Chromium
             const browser = await puppeteer.launch({
                 args: chromium.args,
                 defaultViewport: chromium.defaultViewport,
@@ -18,17 +43,32 @@ class Email {
                 headless: chromium.headless,
                 ignoreHTTPSErrors: true,
             });
+
+            // Create a new page in the browser
             const page = await browser.newPage();
+
+            // Set the page content to the rendered HTML
             await page.setContent(html, { waitUntil: 'networkidle0' });
+
+            // Generate a PDF from the page
             const pdfBuffer = await page.pdf();
+
+            // Close the browser
             await browser.close();
+
+            // Send the PDF buffer as a response
+            res.set({
+                'Content-Type': 'application/pdf',
+                'Content-Disposition': 'attachment; filename=email.pdf',
+            });
             res.send(pdfBuffer);
 
         } catch (error) {
-            console.error(error);
-            res.send(error);
+            console.error('Error generating PDF:', error);
+            res.status(500).send('An error occurred while generating the PDF.');
         }
     }
+
 }
 
 module.exports = new Email();
